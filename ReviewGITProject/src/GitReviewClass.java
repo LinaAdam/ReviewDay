0e1e8ff84e4911cf6d0126420485d931929ed271
LinaAdam <linaadam762@gmail.com>
2020-02-17 22:16:46 -0500

    merge

--- ReviewGITProject/src/GitReviewClass.java
+++ ReviewGITProject/src/GitReviewClass.java
@@ -4,8 +4,9 @@ public class GitReviewClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Review Day");
-		System.out.println("Review Day locally")
-			Sysout.out.println("Review Day")
+		System.out.println("Review Day locally");
+		System.out.println("Review Day");
+		System.out.println("Review Day");
 	}
 
 }
