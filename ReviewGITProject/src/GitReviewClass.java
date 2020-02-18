8ba74a484bd767168ea2d61b7a78cf51401fed84
LinaAdam <60907791+LinaAdam@users.noreply.github.com>
2020-02-17 21:34:14 -0500

    Update GitReviewClass.java

--- ReviewGITProject/src/GitReviewClass.java
+++ ReviewGITProject/src/GitReviewClass.java
@@ -5,7 +5,7 @@ public class GitReviewClass {
 		// TODO Auto-generated method stub
 		System.out.println("Review Day");
 		System.out.println("Review Day locally")
-			
+			Sysout.out.println("Review Day")
 	}
 
 }
